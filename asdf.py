17b6f09c401e818fa53fa3e8c316cdf7e84f177c
pedrozap3005-source <pedrozap3005@gmail.com>
2026-08-13 22:03:09 -0300

    Add files via upload
    Operações básicas

---
+++ asdf.py
@@ -0,0 +1,14 @@
+print("--- operações Aritméticas")
+a = 10
+b = 3
+
+soma = a + b    #Soma
+subtracao = a - b   #Subtração
+multiplicacao = a * b   #Multiplicação
+divisao = a / b     #Divisão (sempre retorna um número com ponto flutuante, ex)
+print(f"{a} + {b} = {soma}")
+print(f"{a} - {b} = {subtracao}")
+print(f"{a} * {b} = {multiplicacao}")
+print(f"{a} / {b} = {divisao}")
+print(f"{a} // {b} = {divisao_inteira}")
+print(f"{a} % {b} = {})
