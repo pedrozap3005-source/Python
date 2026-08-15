1d33ec445089207c4b6f9eeeb4761f1d0c470c1b
pedrozap3005-source <pedrozap3005@gmail.com>
2026-08-15 17:24:56 -0300

    Add files via upload
    Equação_4

---
+++ equacao_04.py
@@ -0,0 +1,26 @@
+
+'''
+Docstring para equacao04
+
+"Multipliquem um número por 3 e subtraia 9. O resultado é igual ao próprionúmero." Qual é esse número?
+
+'''
+
+# Problema: "Multiplique um número por 3 e subtraia 9. O resultado é igual ao próprio número."
+# Equação: 3x - 9 = x
+
+# Resolvendo a equação:
+# 3x - 9 = x
+# 3x - x = 9
+# 2x = 9
+# x = 9 / 2
+# x = 4.5
+
+numero = 4.5
+
+# Realizando a operação descrita no problema
+resultado = (3 * numero) - 9
+
+print(f"O número procurando é {numero}.")
+print(f"Verificação: (3 * {numero} - 9 = {resultado})")
+print(f"O resultado  ({resultado}) é igual ao número original ({numero}).")
