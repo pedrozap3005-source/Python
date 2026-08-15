7f31f1972b6e0b6cc8aa2d16a7ad7fe76862ec8f
pedrozap3005-source <pedrozap3005@gmail.com>
2026-08-15 17:55:35 -0300

    Add files via upload
    Equação_7

---
+++ esquacao_07.py
@@ -0,0 +1,24 @@
+
+'''
+
+Qual número atente ao seguinte critério? "Sua metade mais 7 é igual a 19."
+
+'''
+
+# Problema: "Sua metade mais 7 é igual a 19."
+#
+# Equação:
+#
+# Resolvendo:
+# x / 2 = 19 - 7
+# x / 2 = 12
+# x = 12 * 2
+# x = 24
+
+numero = 24
+
+# Verificando a condição
+resultado = (numero / 2) + 7
+
+print(f"O número que atende ao critério é {numero}.")
+print(f"Verificação: a metade de {numero} ({numero / 2}) mais 7 é igual a {resultado}.")
