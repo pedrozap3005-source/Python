76e1580fce362bc0496aecb3e71e948baac872ae
pedrozap3005-source <pedrozap3005@gmail.com>
2026-08-15 17:47:52 -0300

    Add files via upload
    Equação_6

---
+++ equacao_06.py
@@ -0,0 +1,25 @@
+
+# Problema: "A soma de dois números consecutivos é 47."
+#
+# Seja o priemrio número = x
+# O número consecutivo a ele = x + 1
+#
+# Equação:
+# x + (x + 1) = 47
+# 2x + 1 = 47
+# 2x = 47 - 1
+# 2x = 46
+# x = 46 / 2
+# x = 23
+
+# O primeiro número é 23.
+numero1 = 23
+# O segundo número consecutivo é 24.
+numero2 = numero1 + 1
+
+# Verificando a soma
+soma = numero1 + numero2
+
+print(f"O primeiro numero é: {numero1}")
+print(f"O segundo número é: {numero2}")
+print(f"A soma dos dois é: {soma}")
